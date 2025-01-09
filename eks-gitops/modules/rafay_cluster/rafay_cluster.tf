452e2122b8adcef1314362a6404171b85ff06421
David.C <david.c@Rafayadmins-MacBook-Pro.local>
2025-01-09 10:05:03 -0700

    found the right eks module

--- eks-gitops/modules/rafay_cluster/rafay_cluster.tf
+++ eks-gitops/modules/rafay_cluster/rafay_cluster.tf
@@ -1,4 +1,4 @@
-resource "rafay_eks_cluster_spec" "cluster" {
+resource "rafay_eks_cluster_spec" "var.cluster_name" {
   name         = var.cluster_name
   projectname  = var.project_name
   yamlfilepath = var.cluster_spec_path
