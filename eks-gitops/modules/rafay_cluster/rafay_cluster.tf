1627bb66052b61442e06f0a8e957975e43db9934
David.C <david.c@Rafayadmins-MacBook-Pro.local>
2025-01-10 13:16:58 -0700

    found the right eks module

--- eks-gitops/modules/rafay_cluster/rafay_cluster.tf
+++ eks-gitops/modules/rafay_cluster/rafay_cluster.tf
@@ -1,72 +1,8 @@
-resource "rafay_eks_cluster" "eks-cluster-1" {
-    cluster {
-    kind = "Cluster"
-    metadata {
-      name    = "eks-cluster-1"
-      project = "david-clark-demo-project"
-    }
-    spec {
-      type           = "eks"
-      blueprint      = "minimal"
-      blueprint_version = "latest"
-      cloud_provider = "eks-role"
-      cni_provider   = "aws-cni"
-      #proxy_config   = {}
-    }
-  }
-  cluster_config {
-    apiversion = "rafay.io/v1alpha5"
-    kind       = "ClusterConfig"
-    metadata {
-      name    = "eks-cluster-1"
-      region  = "us-west-2"
-      version = "latest"
-    }
-    iam {
-      with_oidc = true
-    }
-    vpc {
-      cidr = "192.168.0.0/16"
-      cluster_endpoints {
-        private_access = true
-        public_access  = true
-      }
-      nat {
-        gateway = "Single"
-      }
-    }
-    node_groups {
-      disable_pods_imds = false
-      name       = "ng-1"
-      ami_family = "AmazonLinux2"
-      iam {
-      }
-      instance_type    = "m5.xlarge"
-      desired_capacity = 1
-      min_size         = 1
-      max_size         = 2
-      max_pods_per_node = 50
-      version          = "latest"
-      volume_size      = 80
-      volume_type      = "gp3"
-      private_networking = true
-      volume_throughput = 125
-      efa_enabled = false
-      volume_iops = 3000
-      disable_imdsv1 = false
-    }
-    addons {
-      name = "vpc-cni"
-      version = "latest"
-    }
-    addons {
-      name = "kube-proxy"
-      version = "latest"
-
-    }
-    addons {
-      name = "coredns"
-      version = "latest"
-    }
-  }
+resource "rafay_eks_cluster_spec" "eks-gitops-cluster2" {
+  name         = var.cluster_name
+  projectname  = var.project_name
+  yamlfilepath = var.cluster_spec_path
+  yamlfileversion = var.cluster_spec_version
+  waitflag = "1"
+  checkdiff = true
 }
